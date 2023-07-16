c89f7047e3c0c20fd472b381acc2c9e9e0f43054
Emmanuel <tobi3t@gmail.com>
2023-07-16 20:29:37 +0100

    Minor Fix

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> <!--BOOTSTRAP 5 CSS STYLESHEET-->
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <a href="logout.php">logout</a>
-    
+<h1 class="display-1">Display 1</h1>
+
+
+  <header>
+    <nav class="top-nav">
+      <ul>
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </nav>
+
+
+    <!-- Footer -->
+    <footer class="footer bg-dark text-white py-4">
+        <div class="container">
+            <div class="text-center">
+                <p class="mb-0">&copy; 2023 MeasureRecovery. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script> <!--BOOTSTRAP 5 JS SCRIPT-->
 </body>
+
 </html>
+
+
