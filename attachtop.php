3a9273d713cdc40e72cbacadcbe43f9e3c688bb9
Emmanuel <tobi3t@gmail.com>
2023-08-06 14:13:05 +0100

    Title Adjustments

--- attachtop.php
+++ attachtop.php
@@ -36,7 +36,7 @@ $conn->close();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Personal Profile</title>
+  <title>MeasureRecovery - Personal Profile</title>
   <!--BOOTSTRAP 5 CSS STYLESHEET-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
