99a02706d788180f40ab13eda82214db1a5a2891
Emmanuel <tobi3t@gmail.com>
2023-08-09 14:24:23 +0100

    Commented adminpdfs.php

--- adminpdfs.php
+++ adminpdfs.php
@@ -12,21 +12,26 @@ include("admintop.php");
                 <h4>List of PDFs</h4>
                 <ul class="list-group">
                     <?php
+                    # define the folder where PDF files are stored
                     $pdf_folder = 'pdfs/';
-                    $pdf_files = scandir($pdf_folder);
+                    # getting a list of files in the PDF folder
+                    $pdf_files = scandir($pdf_folder); # https://www.php.net/manual/en/function.scandir.php
 
+                    # looping through the files in the PDF folder
                     foreach ($pdf_files as $pdf_file) {
                         if ($pdf_file === '.' || $pdf_file === '..') {
                             continue;
                         }
-
+                        # checking if the file has a .pdf extension
                         $file_extension = strtolower(pathinfo($pdf_folder . $pdf_file, PATHINFO_EXTENSION));
                         if ($file_extension === 'pdf') {
                             echo '<li class="list-group-item">';
                             echo '<div class="d-flex align-items-center">';
+                            # embeding the PDF for preview
                             echo '<embed src="' . $pdf_folder . $pdf_file . '" width="500" height="500" type="application/pdf">';
                             echo '<span class="ms-3">' . $pdf_file . '</span>';
                             echo '</div>';
+                            # providing a link to delete the PDF
                             echo '<a href="delete_pdf.php?pdf=' . urlencode($pdf_file) . '" class="btn btn-danger ms-3">Delete</a>';
                             echo '</li>';
                         }
@@ -42,6 +47,7 @@ include("admintop.php");
     <div class="row">
         <div class="col-12">
             <h2>Upload PDF</h2>
+            <!-- Form to upload a new PDF file -->
             <form action="upload_pdf.php" method="post" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="pdfFile" class="form-label">Select PDF file to upload:</label>
