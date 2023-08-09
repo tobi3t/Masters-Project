f62cede8cbab6e05d832084d994d5d17eb8d0f16
Emmanuel <tobi3t@gmail.com>
2023-08-09 16:11:32 +0100

    Comments added

--- delete_article.php
+++ delete_article.php
@@ -1,20 +1,20 @@
 <?php
 include("connection.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+# getting the article ID from the POST request
 $article_id = $_POST["article_id"];
 
+# preparing the DELETE query to remove the article with the given ID
 $stmt = $conn->prepare("DELETE FROM articles WHERE id = ?");
 $stmt->bind_param("i", $article_id);
 
+# executing the DELETE query and check if it was successful
 if ($stmt->execute()) {
+    // if successful, redirect to the admin articles page
     header('Location: adminarticles.php');
 } else {
+    // if there was an error, display an error message
     echo '<div class="alert alert-danger" role="alert">Error deleting the article: ' . $stmt->error . '</div>';
 }
 
--- delete_pdf.php
+++ delete_pdf.php
@@ -1,16 +1,24 @@
 <?php
+# checking if a 'pdf' parameter is set in the URL
 if (isset($_GET['pdf'])) {
+    # retrieving the PDF filename from the URL parameter
     $pdf_file = $_GET['pdf'];
+    # constructing the full path to the PDF file
     $pdf_path = 'pdfs/' . $pdf_file;
 
+    # checking if the PDF file exists
     if (file_exists($pdf_path)) {
+        # if the file exists, delete it using the unlink() function
         unlink($pdf_path);
+        # redirecting back to the admin PDFs page after deleting the file
         header('Location: adminpdfs.php');
-        exit;
+        exit; # terminate script execution
     } else {
+        # if the file does not exist, display an error message
         echo "File not found.";
     }
 } else {
+    # if the 'pdf' parameter is not set in the URL, display an invalid request message
     echo "Invalid request.";
 }
 ?>
