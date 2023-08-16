72f0e5fca15761ee5c03c333d444329f41a093e7
Emmanuel <123545882+tobi3t@users.noreply.github.com>
2023-08-16 10:21:49 +0100

    Adjustments

--- connection.php
+++ connection.php
@@ -1,16 +1,16 @@
 <?php
 # database connection details
-$servername = "localhost";  # Server where the database is hosted
-$username = "root";         # Username to access the database
-$password = "";             # Password to access the database
-$dbname = "gamify";         # Name of the database to connect to
+$servername = "localhost";  # server where the database is hosted
+$username = "root";         # username to access the database
+$password = "";             # password to access the database
+$dbname = "gamify";         # name of the database to connect to
 
 # creating a new MySQLi instance to establish a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 # checking if the connection was successful
 if ($conn->connect_error) {
-  # If connection fails, display an error message and terminate the script
+  # if connection fails, display an error message and terminate the script
   die("Connection failed: " . $conn->connect_error);
 }
 ?>
