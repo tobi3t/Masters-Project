0fee3dc0dafff67b7e42fb5981979b28b09dd07f
Emmanuel <123545882+tobi3t@users.noreply.github.com>
2023-08-17 03:40:57 +0100

    Adjustment

--- database_tables/chat_messages.sql
+++ database_tables/chat_messages.sql
@@ -3,10 +3,7 @@ CREATE TABLE chat_messages (
     content TEXT NOT NULL,
     user_id INT NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    category_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
 );
-
-
-ALTER TABLE chat_messages
-ADD COLUMN category_id INT,
-ADD FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE;
